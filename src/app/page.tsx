833667acb306b974dc472a65f36557fd86915dc2
ArtemKhoroshavin <horoshavin2012@yande.ru>
2024-07-08 01:09:15 +0300

    br5

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+import Main from "@components/Main/Main";
+
+export default function Home() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <Main />
+      </div>
+    </div>
+
+  );
+}
