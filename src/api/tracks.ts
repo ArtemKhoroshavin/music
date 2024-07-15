994feabe4cc4a6b9635b6f960a3b6c80593bc635
ArtemKhoroshavin <horoshavin2012@yande.ru>
2024-07-16 00:02:39 +0300

    33

--- src/api/tracks.ts
+++
@@ -1,9 +0,0 @@
-export async function getTracks() {
-    const res = await fetch(
-        "https://skypro-music-api.skyeng.tech/catalog/track/all/"
-    );
-      if (!res.ok) {
-        throw new Error("Ошибка при получении данных");
-    }
-    return res.json();
-}     
